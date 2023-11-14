10bd30a72134a146772524c37160e365228ef303
siddhi-lipare <siddhi21@iiserb.ac.in>
2023-11-14 21:15:53 +0530

    reqs added

--- main_classification.py
+++ main_classification.py
@@ -3,5 +3,5 @@ from classification import Classification
 import warnings
 warnings.filterwarnings("ignore")
 
-classifier = Classification(clf_opt='ab', impute_opt='knn', no_of_selected_features=1000)
+classifier = Classification(clf_opt='dt', impute_opt='mean', no_of_selected_features=1000)
 classifier.classification()
