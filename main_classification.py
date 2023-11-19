d170bf52cd930b3757d09adf1e6d476a3235a338
Siddhi Lipare <98029886+siddhi-lipare@users.noreply.github.com>
2023-11-19 20:48:17 +0530

    Delete main_classification.py

--- main_classification.py
+++
@@ -1,7 +0,0 @@
-from classification import Classification
-
-import warnings
-warnings.filterwarnings("ignore")
-
-classifier = Classification(clf_opt='ab', impute_opt='knn')
-classifier.classification()
