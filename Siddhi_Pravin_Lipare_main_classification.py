2f5f66400118dc431f6eafa4610334d288d5aa91
Siddhi Lipare <98029886+siddhi-lipare@users.noreply.github.com>
2023-11-19 23:00:15 +0530

    Update Siddhi_Pravin_Lipare_main_classification.py

--- Siddhi_Pravin_Lipare_main_classification.py
+++ Siddhi_Pravin_Lipare_main_classification.py
@@ -3,5 +3,5 @@ from Siddhi_Pravin_Lipare_classification import Classification
 import warnings
 warnings.filterwarnings("ignore")
 
-classifier = Classification(clf_opt='xg', impute_opt='knn')
+classifier = Classification(clf_opt='ab', impute_opt='knn')
 classifier.classification()
